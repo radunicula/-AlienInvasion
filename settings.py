3ef467086725eee048398dc3d44e96ffb8655aea
radunicula <radu7.nicula@gmail.com>
2023-01-20 20:19:06 +0200

    build alien fleet

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings.
-        self.ship_speed = 5
+        self.ship_speed = 1.5
 
         # Bullet settings
         self.bullet_speed = 2.0
@@ -17,3 +17,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings.
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
