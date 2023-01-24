5192738fdf744fd442183bf229db325c4fbcff93
radunicula <radu7.nicula@gmail.com>
2023-01-24 21:39:07 +0200

    shooting aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -56,7 +56,7 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_SPACE:
-            self._fire_bullet()
+            self._fire_bullet()            
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -80,6 +80,20 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        self._check_bullet_alien_collisions()
+        
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
--- ship.py
+++ ship.py
@@ -31,7 +31,7 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-
+            
         # Update rect object from self.x.
         self.rect.x = self.x
 
